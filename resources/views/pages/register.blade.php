402e83618734594744f0f0e7ba6db01085646f06
FikFikk <fikri225456@gmail.com>
2023-10-28 00:29:46 +0700

    register user

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -23,15 +23,16 @@
             </div>
             <!-- END: Register Info -->
             <!-- BEGIN: Register Form -->
-            <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
+            <form action="{{ route('register.store') }}" method="POST" enctype="multipart/form-data" class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
+                @csrf
                 <div class="my-auto mx-auto xl:ml-20 bg-white dark:bg-darkmode-600 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
                     <h2 class="text-white intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">Sign Up</h2>
                     <div class="intro-x mt-2 text-slate-400 dark:text-slate-400 xl:hidden text-center">A few more clicks to sign in to your account. Manage all your e-commerce accounts in one place</div>
                     <div class="intro-x mt-8">
-                        <input type="text" class="intro-x login__input form-control py-3 px-4 block" placeholder="First Name">
-                        <input type="text" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Last Name">
-                        <input type="text" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Email">
-                        <input type="password" class="intro-x login__input form-control py-3 px-4 block mt-4 "id="password" placeholder="Password">
+                        <input type="text" name="name" id="name" class="intro-x login__input form-control py-3 px-4 block" placeholder="Fullname">
+                        <input type="text" name="email" id="email" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Email">
+                        <input type="text" name="phone" id="phone" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Phone">
+                        <input type="password" name="password" id="password" class="intro-x login__input form-control py-3 px-4 block mt-4 "id="password" placeholder="Password">
                         <div class="intro-x w-full grid grid-cols-12 gap-1 h-1 mt-3" id="passwordStrengthIndicator">
                             <div class="col-span-3 h-full rounded bg-slate-100 dark:bg-darkmode-800"></div>
                             <div class="col-span-3 h-full rounded bg-slate-100 dark:bg-darkmode-800"></div>
@@ -42,7 +43,6 @@
                         <!-- <a href="" class="intro-x text-slate-500 block mt-2 text-xs sm:text-sm">What is a secure password?</a> -->
                         <input type="password" class="intro-x login__input form-control py-3 px-4 block mt-4" id="confirmPassword" placeholder="Password Confirmation">
                         <div id="passwordMismatchMessage" class="text-white text-sm mt-2 hidden">Password confirmation does not match.</div>
-
                     </div>
                     <div class="intro-x flex items-center text-slate-600 dark:text-slate-500 mt-4 text-xs sm:text-sm">
                         <input id="remember-me" type="checkbox" class="form-check-input border mr-2">
@@ -54,7 +54,7 @@
                         <button class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top"><a href="{{ route('login.index') }}">Sign in</a></button>
                     </div>
                 </div>
-            </div>
+            </form>
             <!-- END: Register Form -->
         </div>
     </div>
